1c4d96fed2ee5395f80c9fbabeebd9a32142237a
kaikaci12 <somewonesomewone@gmail.com>
2024-03-14 21:07:31 +0400

    change the close png and set position to fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ burgerMeniu.addEventListener("click", () => {
   navigation.classList.toggle("navigationActive");
   toggle = !toggle;
   if (toggle) {
-    burgerMeniu.setAttribute("src", "./images/close.png");
+    burgerMeniu.setAttribute("src", "./images/close-icon-13582.png");
   } else {
     burgerMeniu.setAttribute("src", "./images/icon-hamburger.svg");
   }
